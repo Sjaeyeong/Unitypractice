32ab768b43d1bf5a0536d01228b52790cdd98dcd
Sjaeyeong <duddl705@naver.com>
2025-03-07 17:54:19 +0900

    aaa

--- Kimchi-Run/Assets/Scripts/BackgroundScroll.cs
+++ Kimchi-Run/Assets/Scripts/BackgroundScroll.cs
@@ -17,6 +17,6 @@ public class BackgroundScroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0);
     }
 }

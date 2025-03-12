eb6feb8e5140c087d6364f9d88835dda4947cf24
Sjaeyeong <duddl705@naver.com>
2025-03-12 16:02:22 +0900

    player jump

--- Kimchi-Run/Assets/Scripts/Player.cs
+++ Kimchi-Run/Assets/Scripts/Player.cs
@@ -6,7 +6,9 @@ public class Player : MonoBehaviour
     public float jumpForce;
 
     [Header("References")]
-    public Rigidbody2D PlyaerRigidBody;
+    public Rigidbody2D PlayerRigidBody;
+
+    private bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +18,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){ 
+            PlayerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) { // OnCollisionEnter2D는 입력된 Collider가 플레이어의 Collider와 부딪히면 호출 됨
+        if(collision.gameObject.name == "Platforms") {
+            isGrounded = true;
+        }
     }
 }

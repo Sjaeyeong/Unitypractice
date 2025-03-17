714c54476c5678189681ddb590d31f4c913ea38e
Sjaeyeong <duddl705@naver.com>
2025-03-17 15:59:56 +0900

    add enemey food

--- Kimchi-Run/Assets/Scripts/Player.cs
+++ Kimchi-Run/Assets/Scripts/Player.cs
@@ -7,9 +7,13 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     public Rigidbody2D PlayerRigidBody;
+
+    public BoxCollider2D PlayerCollider;
     public Animator PlayerAnimator;
 
     private bool isGrounded = true;
+    public int lives = 3;
+    public bool isInvincible = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,12 +30,65 @@ public class Player : MonoBehaviour
         }
     }
 
+    void KillPlayer()
+    {
+        PlayerCollider.enabled = false;
+        PlayerAnimator.enabled = false;
+        PlayerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
+    }
+
     void OnCollisionEnter2D(Collision2D collision) { // OnCollisionEnter2D는 입력된 Collider가 플레이어의 Collider와 부딪히면 호출 됨
         if(collision.gameObject.name == "Platforms") {
             if (!isGrounded) { // 게임을 막 시작했을 땐 state를 2로 바꾸고 싶지 않기 때문에 조건문 추가
                 PlayerAnimator.SetInteger("state", 2);
             }
             isGrounded = true;
+        }
+    }
+
+    void Hit()
+    {
+        lives -= 1;
+        if(lives == 0)
+        {
+            KillPlayer();
+        }
+    }
+
+    void Heal()
+    {
+        lives = Mathf.Min(3, lives + 1); // 어떤 이유든지 3으로 설정 = 3과 lives+1중 최소로 반환
+    }
+
+    void StartInvincible()
+    {
+        isInvincible = true;
+        Invoke("StopInvincible", 5f);
+    }
+
+    void StopInvincible()
+    {
+        isInvincible = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "enemy")
+        {
+            Destroy(collider.gameObject);
+            if(!isInvincible) {
+                Hit();
+            }
+        }
+        else if(collider.gameObject.tag == "food")
+        {
+            Destroy(collider.gameObject);
+            Heal();
+        }
+        else if(collider.gameObject.tag == "golden")
+        {
+            Destroy(collider.gameObject);
+            StartInvincible();
         }
     }
 }
--- Kimchi-Run/Assets/Scripts/Spawner.cs
+++ Kimchi-Run/Assets/Scripts/Spawner.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("Settings")]
+    public float minSpawnDelay;
+    public float maxSpawnDelay;
+
     [Header("References")]
     public GameObject[] gameObjects;    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
     }
 
     void Spawn() {
         GameObject randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
         Instantiate(randomObject, transform.position, Quaternion.identity);
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay)); // min과 max사이의 초마다 호출
     }
 }

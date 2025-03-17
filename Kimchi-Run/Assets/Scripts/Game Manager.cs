32eeaa954dda0308ed930ead69d0fd3408784432
Sjaeyeong <duddl705@naver.com>
2025-03-17 16:03:41 +0900

    Update Game Manager.cs

--- Kimchi-Run/Assets/Scripts/Game Manager.cs
+++ Kimchi-Run/Assets/Scripts/Game Manager.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
+    void Awake() // start보다 먼저 유니티에 의해 실행되는 함수
+    {
+        if(Instance == null) // gamemanger 인스턴스를 다른 곳에서도 공유해서 사용할 수 있음
+        {
+            Instance = this;
+        }   
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

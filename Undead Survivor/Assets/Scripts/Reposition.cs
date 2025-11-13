7366ec8515bd83e8674cd430db7ce9b55e66504b
Sjaeyeong <duddl705@naver.com>
2025-11-13 18:15:41 +0900

    tileExtense

--- Undead Survivor/Assets/Scripts/Reposition.cs
+++ Undead Survivor/Assets/Scripts/Reposition.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void OnTriggerExit2D(Collider2D collision)
+  {
+    if (!collision.CompareTag("Area"))
+      return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+      Vector3 playerPos = GameManager.instance.player.transform.position;
+      Vector3 myPos = transform.position;
+      float diffx = Mathf.Abs(playerPos.x - myPos.x);
+      float diffy = Mathf.Abs(playerPos.y - myPos.y);
+
+      Vector3 playerDir = GameManager.instance.player.inputVec;
+      float dirX = playerDir.x < 0 ? -1 : 1;
+      float dirY = playerDir.y < 0 ? -1 : 1;
+
+      switch (transform.tag)
+        {
+        case "Ground":
+            if (diffx > diffy)
+            {
+                transform.Translate(Vector3.right * dirX * 40);
+            }
+            else if (diffx < diffy)
+            {
+                transform.Translate(Vector3.up * dirY * 40);
+            }
+            break;
+        case "Enemy":
+
+            break;
+        }
+  }
 }
